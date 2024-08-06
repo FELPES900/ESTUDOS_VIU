9a8041f7ee612381df68c74f9664dafada9d0ca4
FELPES900 <felipefraga.assis@gmail.com>
2024-08-06 20:56:55 -0300

    feat: Entendo as posibilidades de fazer dentro do html de maneira dinamica

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <h1>Ola a todos</h1>
-  <h2>{{saludo}}</h2>
+  <h2>{{saludo.toUpperCase()}}</h2>
   {{ buttonHtml }}
   <span v-html="buttonHtml"></span> <!-- Pegar a string que foi gerado e faz a inserção no html-->
   <p v-bind:style="color">TESTE</p> <!-- Passando uma variavel para o v-bind:style -->
   <a v-bind:href="website">Google</a> <!-- Passando uma variavel para o v-bind:href n-->
+  {{ 2*9 }}
+  <h2 v-bind:style="`color :${ahorras >= 200 ? 'green' : 'red'}`">${{ ahorras }}</h2> <!-- maneiras de colocar o CSS dentro do HTML -->
+  <h2 v-bind:class="ahorras >= 200 ? 'conFondo' : 'sinFondo'">${{ ahorras }}</h2>
 </template>
 
 <script setup lang="ts">
@@ -12,4 +15,5 @@
   const buttonHtml = '<button>Enviar</button>'
   const color = "color: red; background-color: black"
   const website = 'https://google.com/'
+  const ahorras = 1500
 </script>
